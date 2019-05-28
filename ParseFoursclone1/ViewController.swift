3ab7f235a7a14e3ed4f5eca283aac280934b605d
MacBookPro <smttdgru34@gmail.com>
2019-05-28 16:20:42 +0300

    Initial Commit

---
+++ ParseFoursclone1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseFoursclone1
+//
+//  Created by MacBookPro on 28.05.2019.
+//  Copyright © 2019 Samet Dogru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
